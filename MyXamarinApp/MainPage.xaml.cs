080da3ecef46a09f0ce3095a82ea27e98a07257c
Arulraj A <arul.raj47@gmail.com>
2020-12-22 17:17:05 +0530

    Committed the example project.

---
+++ MyXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.Data;
+using Syncfusion.SfDataGrid.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
